1c0bbdf7db84e76ec473838b8883a3264c8216a0
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-04-26 17:06:21 -0700

    omdb function fully working

--- liri.js
+++ liri.js
@@ -1,7 +1,9 @@
 // activates .env file
 require("dotenv").config()
 
+// this allows us to use the node js module
 const fs = require('fs')
+
 const axios = require('axios')
 
 // activates keys stored in keys.js file
@@ -13,31 +15,42 @@ console.log(name)
 // OMDB Function
 let movieThis = _ => {
 axios.get(`http://www.omdbapi.com/?t=${movie}&apikey=trilogy`)
-.then(({data}) => console.log(data))
+.then(d => {
+    // console.log(d)
+    console.log(`
+    Title: ${d.data.Title}
+    Year: ${d.data.Year}
+    IMDB Rating: ${d.data.imdbRating}
+    Country: ${d.data.Country}
+    Language: ${d.data.Language}
+    Actors: ${d.data.Actors}
+    Plot: ${d.data.Plot}
+    `)
+})
 .catch(e => console.log(e))
-}
+} 
+
 if (name === 'movieThis') {
     movieThis()
-}
+} 
+// // spotify npm
+// let Spotify = require('node-spotify-api')
 
-// spotify npm
-let Spotify = require('node-spotify-api')
+// let spotify = new Spotify({
+//     id: process.env.SPOTIFY_ID,
+//     secret: process.env.SPOTIFY_SECRET
+// })
 
-let spotify = new Spotify({
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
-})
-
-let spotifyThisSong = _ => {
-spotify
-    .search({type: 'track', query: 'All the Small Things'})
-    .then(function(response){
-        console.log(response)
-    })
-    .catch(function(err){
-        console.log(err)
-    })
-}
+// let spotifyThisSong = _ => {
+// spotify
+//     .search({type: 'track', query: 'All the Small Things'})
+//     .then(function(response){
+//         console.log(response)
+//     })
+//     .catch(function(err){
+//         console.log(err)
+//     })
+// }
 
 // Show following info about song in terminal
     // Artist
