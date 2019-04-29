bb6a980389e6d78ae59c8524ba4ce5424c2de5d9
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-04-28 22:37:00 -0700

    spotify working, few more adj

--- liri.js
+++ liri.js
@@ -9,48 +9,58 @@ const axios = require('axios')
 const keys = require("./keys.js");
 
 const Spotify = require('node-spotify-api')
+
 let spotify = new Spotify({
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 })
 
-let [, , name, movie] = process.argv
-console.log(name)
+let [, , action, title] = process.argv
+console.log(action)
 
 // OMDB Function
 let movieThis = _ => {
-axios.get(`http://www.omdbapi.com/?t=${movie}&apikey=trilogy`)
-.then(d => {
-    // console.log(d)
-    console.log(`
-    Title: ${d.data.Title}
-    Year: ${d.data.Year}
-    IMDB Rating: ${d.data.imdbRating}
-    Country: ${d.data.Country}
-    Language: ${d.data.Language}
-    Actors: ${d.data.Actors}
-    Plot: ${d.data.Plot}
-    `)
-})
-.catch(e => console.log(e))
+    axios.get(`http://www.omdbapi.com/?t=${title}&apikey=trilogy`)
+        .then(d => {
+            // console.log(d)
+            console.log(`
+            Title: ${d.data.Title}
+            Year: ${d.data.Year}
+            IMDB Rating: ${d.data.imdbRating}
+            Country: ${d.data.Country}
+            Language: ${d.data.Language}
+            Actors: ${d.data.Actors}
+            Plot: ${d.data.Plot}
+            `)
+        })
+        .catch(e => console.log(e))
 } 
 
-if (name === 'movieThis') {
+if (action === 'movieThis') {
     movieThis()
 } 
 
 // Spotify Function
 let spotifyThisSong = _ => {
-spotify.search({type: 'track', query: `${song}`})
-    .then(r => {
-        console.log(r)
-    })
-    .catch(e => console.log(e))
+    spotify.search({type: 'track', query: `${title}`, limit: 1})
+        .then(r => {
+            console.log(r.tracks.items[0].type)
+        })
+        .catch(e => console.log(e))
 }
 
-if (name === 'spotifyThisSong') {
+if (action === 'spotifyThisSong') {
     spotifyThisSong()
 } 
+
+// // Band is Town Function
+// let concertThis = _ => {
+//     axios.get(`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`)
+//     .then(d => {
+//         console.log(d)
+//     })
+//     .catch(e => console.log(e))
+// }
 
 
 
